061d948cde74edc3ab76a728eab5a31894b3536b
Naz Hacıhafızoğlu <nazhacihafizoglu@gmail.com>
2024-06-15 02:35:10 +0300

    feat: Add LoginRegister component with login and register forms

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -1,36 +1,115 @@
-import React from "react";
-import { Input, Button } from "antd"; // Ant Design kütüphanesi kullanıldığı varsayılarak import edildi
+// src/components/LoginRegister.js
+import React, { useState } from "react";
+import { Tabs, Form, Input, Button, Checkbox } from "antd";
 
-const RegisterPage = () => {
+const { TabPane } = Tabs;
+
+const LoginRegister = () => {
+  // Sekmeler arasında geçişi yönetmek için state (durum) tanımlanıyor
+  const [activeTab, setActiveTab] = useState("1");
+
+  // Sekme değişikliklerini yönetmek için fonksiyon tanımlanıyor
+  const onTabChange = (key) => {
+    setActiveTab(key);
+  };
+
   return (
-    <div>
-      <label>Adınız:</label>
-      <Input placeholder="Ad" type="text" style={{ width: 200, height: 30 }} />
-      <br />
-      <label>Soyadınız:</label>
-      <Input
-        placeholder="Soyad"
-        type="text"
-        style={{ width: 200, height: 30 }}
-      />
-      <br />
-      <label>E-mail Adresiniz:</label>
-      <Input
-        placeholder="E-mail"
-        type="email"
-        style={{ width: 200, height: 30 }}
-      />
-      <br />
-      <label>Şifreniz:</label>
-      <Input
-        placeholder="Şifre"
-        type="password"
-        style={{ width: 200, height: 30 }}
-      />
-      <br />
-      <Button type="primary">Kayıt Olun</Button>
+    <div
+      style={{
+        width: 300, // Genişlik
+        margin: "auto", // Ortalamak için margin
+        padding: 20, // İç boşluk
+        boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)", // Gölgeleme
+        backgroundColor: "white", // Arka plan rengi
+        borderRadius: 10, // Kenar yuvarlaklığı
+      }}
+    >
+      {/* Sekmeler (Login ve Register) */}
+      <Tabs defaultActiveKey="1" activeKey={activeTab} onChange={onTabChange}>
+        <TabPane tab="Login" key="1">
+          {/* Login Formu */}
+          <Form name="login" initialValues={{ remember: true }}>
+            <Form.Item
+              name="username"
+              rules={[
+                { required: true, message: "Please input your Username!" },
+              ]}
+            >
+              <Input placeholder="Username" />
+            </Form.Item>
+
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: "Please input your Password!" },
+              ]}
+            >
+              <Input type="password" placeholder="Password" />
+            </Form.Item>
+
+            <Form.Item name="remember" valuePropName="checked">
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                style={{ width: "100%" }}
+              >
+                Login
+              </Button>
+            </Form.Item>
+          </Form>
+        </TabPane>
+        <TabPane tab="Register" key="2">
+          {/* Register Formu */}
+          <Form name="register" initialValues={{ remember: true }}>
+            <Form.Item
+              name="username"
+              rules={[
+                { required: true, message: "Please input your Username!" },
+              ]}
+            >
+              <Input placeholder="Username" />
+            </Form.Item>
+
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Email!",
+                  type: "email",
+                },
+              ]}
+            >
+              <Input placeholder="Email" />
+            </Form.Item>
+
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: "Please input your Password!" },
+              ]}
+            >
+              <Input type="password" placeholder="Password" />
+            </Form.Item>
+
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                style={{ width: "100%" }}
+              >
+                Register
+              </Button>
+            </Form.Item>
+          </Form>
+        </TabPane>
+      </Tabs>
     </div>
   );
 };
 
-export default RegisterPage;
+export default LoginRegister;
