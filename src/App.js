88511f2965aaf7089b170110063e1f15d4bbc453
Naz Hacıhafızoğlu <nazhacihafizoglu@gmail.com>
2024-06-18 03:33:35 +0300

    background added.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginRegisterPage from "./pages/LoginRegisterPage";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
-    <div>
-      <h1>SpendSmart'a Hoş geldiniz.</h1>
-      <LoginRegisterPage /> {}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginRegisterPage />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </Router>
   );
 };
 
--- src/pages/LoginRegisterPage.js
+++ src/pages/LoginRegisterPage.js
@@ -1,26 +1,26 @@
 import React, { useState } from "react";
 import { Tabs, Form, Input, Button, Checkbox, message } from "antd";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import "./LoginRegisterPage.css";
 
 const { TabPane } = Tabs;
 
 const LoginRegisterPage = () => {
   const [activeTab, setActiveTab] = useState("1");
+  const navigate = useNavigate();
 
   const onTabChange = (key) => {
     setActiveTab(key);
   };
 
   const onRegisterFinish = async (values) => {
+    const endpointUrl =
+      "https://v1.nocodeapi.com/nazhhoglu/google_sheets/BgMLnNnsWvDbVuLw";
     try {
-      const response = await axios.post(
-        "https://v1.nocodeapi.com/nazhhoglu/google_sheets/aPbHKZGeCyToqPsU",
-        {
-          values: [
-            [values.name, values.surname, values.email, values.password],
-          ],
-        }
-      );
+      const response = await axios.post(endpointUrl, {
+        values: [[values.name, values.surname, values.email, values.password]],
+      });
 
       if (response.status === 200) {
         message.success("Registration successful!");
@@ -29,125 +29,147 @@ const LoginRegisterPage = () => {
       }
     } catch (error) {
       console.error("Registration error:", error);
-      message.error("An error occurred during registration. Please try again.");
+      message.error(`An error occurred during registration: ${error.message}`);
     }
   };
 
   const onLoginFinish = async (values) => {
-    // Giriş işlemini gerçekleştirecek kod burada olacak
+    const endpointUrl =
+      "https://v1.nocodeapi.com/nazhhoglu/google_sheets/BgMLnNnsWvDbVuLw";
+    try {
+      const response = await axios.get(endpointUrl);
+
+      if (response.status === 200) {
+        const users = response.data.data;
+        const user = users.find((user) => user[2] === values.email);
+
+        if (user) {
+          if (user[3] === values.password) {
+            message.success("Login successful!");
+            navigate("/home");
+          } else {
+            message.error("Incorrect password.");
+          }
+        } else {
+          message.error("Email not registered.");
+        }
+      } else {
+        message.error("Failed to fetch users. Please try again.");
+      }
+    } catch (error) {
+      console.error("Login error:", error);
+      message.error(`An error occurred during login: ${error.message}`);
+    }
   };
 
   return (
-    <div
-      style={{
-        width: 300,
-        margin: "auto",
-        padding: 20,
-        boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
-        backgroundColor: "white",
-        borderRadius: 10,
-      }}
-    >
-      <Tabs defaultActiveKey="1" activeKey={activeTab} onChange={onTabChange}>
-        <TabPane tab="Login" key="1">
-          <Form
-            name="login"
-            initialValues={{ remember: true }}
-            onFinish={onLoginFinish}
-          >
-            <Form.Item
-              name="email"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your Email!",
-                  type: "email",
-                },
-              ]}
+    <div className="login-register-container">
+      {" "}
+      {/* Yeni eklenen class */}
+      <div className="login-form-container">
+        {" "}
+        {/* Yeni eklenen class */}
+        <Tabs defaultActiveKey="1" activeKey={activeTab} onChange={onTabChange}>
+          <TabPane tab="Login" key="1">
+            <Form
+              name="login"
+              initialValues={{ remember: true }}
+              onFinish={onLoginFinish}
             >
-              <Input placeholder="Email" />
-            </Form.Item>
-
-            <Form.Item
-              name="password"
-              rules={[
-                { required: true, message: "Please input your Password!" },
-              ]}
-            >
-              <Input type="password" placeholder="Password" />
-            </Form.Item>
-
-            <Form.Item name="remember" valuePropName="checked">
-              <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-
-            <Form.Item>
-              <Button
-                type="primary"
-                htmlType="submit"
-                style={{ width: "100%" }}
+              <Form.Item
+                name="email"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your Email!",
+                    type: "email",
+                  },
+                ]}
               >
-                Login
-              </Button>
-            </Form.Item>
-          </Form>
-        </TabPane>
-        <TabPane tab="Register" key="2">
-          <Form
-            name="register"
-            initialValues={{ remember: true }}
-            onFinish={onRegisterFinish}
-          >
-            <Form.Item
-              name="name"
-              rules={[{ required: true, message: "Please input your Name!" }]}
-            >
-              <Input placeholder="Name" />
-            </Form.Item>
+                <Input placeholder="Email" />
+              </Form.Item>
 
-            <Form.Item
-              name="surname"
-              rules={[
-                { required: true, message: "Please input your Surname!" },
-              ]}
-            >
-              <Input placeholder="Surname" />
-            </Form.Item>
-
-            <Form.Item
-              name="email"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your Email!",
-                  type: "email",
-                },
-              ]}
-            >
-              <Input placeholder="Email" />
-            </Form.Item>
-
-            <Form.Item
-              name="password"
-              rules={[
-                { required: true, message: "Please input your Password!" },
-              ]}
-            >
-              <Input type="password" placeholder="Password" />
-            </Form.Item>
-
-            <Form.Item>
-              <Button
-                type="primary"
-                htmlType="submit"
-                style={{ width: "100%" }}
+              <Form.Item
+                name="password"
+                rules={[
+                  { required: true, message: "Please input your Password!" },
+                ]}
               >
-                Register
-              </Button>
-            </Form.Item>
-          </Form>
-        </TabPane>
-      </Tabs>
+                <Input type="password" placeholder="Password" />
+              </Form.Item>
+
+              <Form.Item name="remember" valuePropName="checked">
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  style={{ width: "100%" }}
+                >
+                  Login
+                </Button>
+              </Form.Item>
+            </Form>
+          </TabPane>
+          <TabPane tab="Register" key="2">
+            <Form
+              name="register"
+              initialValues={{ remember: true }}
+              onFinish={onRegisterFinish}
+            >
+              <Form.Item
+                name="name"
+                rules={[{ required: true, message: "Please input your Name!" }]}
+              >
+                <Input placeholder="Name" />
+              </Form.Item>
+
+              <Form.Item
+                name="surname"
+                rules={[
+                  { required: true, message: "Please input your Surname!" },
+                ]}
+              >
+                <Input placeholder="Surname" />
+              </Form.Item>
+
+              <Form.Item
+                name="email"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your Email!",
+                    type: "email",
+                  },
+                ]}
+              >
+                <Input placeholder="Email" />
+              </Form.Item>
+
+              <Form.Item
+                name="password"
+                rules={[
+                  { required: true, message: "Please input your Password!" },
+                ]}
+              >
+                <Input type="password" placeholder="Password" />
+              </Form.Item>
+
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  style={{ width: "100%" }}
+                >
+                  Register
+                </Button>
+              </Form.Item>
+            </Form>
+          </TabPane>
+        </Tabs>
+      </div>
     </div>
   );
 };
