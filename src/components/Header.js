7d7c52dd1a43d50090028a8ad96ed20a56685ff3
Naz Hacıhafızoğlu <nazhacihafizoglu@gmail.com>
2024-07-10 03:41:33 +0300

    edit

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,11 +21,6 @@ const styles = {
 const HeaderBar = () => (
   <Header style={styles.header}>
     <div style={styles.logo}>$pend$mart</div>
-    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
-      <Menu.Item key="1">Dashboard</Menu.Item>
-      <Menu.Item key="2">Search</Menu.Item>
-      <Menu.Item key="3">Profile</Menu.Item>
-    </Menu>
   </Header>
 );
 
